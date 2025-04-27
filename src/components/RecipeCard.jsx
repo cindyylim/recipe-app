d98d1f78480a5c6ef57c2586c7b58d8c99f7a8a0
Cindy <cindyl23@proton.me>
2025-04-27 15:25:28 -0700

    add fetch query to get the recipes based on search query

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,11 @@
-import { Search, Soup, Heart, HeartPulse } from "lucide-react";
+import { Heart, Video } from "lucide-react";
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe }) => {
   return (
     <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
       <a href="#" className="relative h-32">
         <img
-          src="/recipe1.png"
+          src={recipe.strMealThumb}
           alt="recipe img"
           className="rounded-md w-full h-full object-cover cursor-pointer"
         />
@@ -13,28 +13,33 @@ const RecipeCard = () => {
           <Heart size={16} className="hover:fill-red-500 hover:text-red-500" />
         </div>
       </a>
-      <div className="flex items-center gap-1 mt-2">
-        <Soup size={16} />
-        <span className="text-sm">4 servings</span>
-      </div>
       <div className="flex mt-1">
-        <p className="font-bold tracking-wide">Roasted Chicken</p>
+        <p className="font-bold tracking-wide">{recipe.strMeal}</p>
       </div>
-      <p className="my-2">Earls Kitchen</p>
-      <div className="flex gap-2 mt-auto">
+      <p className="my-2">{recipe.strArea}</p>
+      {recipe.strSource && (
+        <div className="flex flex-col gap-2 mt-auto">
+          <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
+            <a
+              className="text-sm tracking-tighter font-semibold"
+              href={recipe.strSource}
+            >
+              {recipe.strSource}
+            </a>
+          </div>
+        </div>
+      )}
+      {recipe.strYoutube && (
         <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
-          <HeartPulse size={16}></HeartPulse>
-          <span className="text-sm tracking-tighter font-semibold">
-            Gluten-free
-          </span>
+          <a
+            className="text-sm tracking-tighter font-semibold"
+            href={recipe.strYoutube}
+          >
+            <Video size={16} />
+            {recipe.strYoutube}
+          </a>
         </div>
-        <div className="flex gap-1  bg-[#d6f497] items-center p-1 rounded-md">
-          <HeartPulse size={16}></HeartPulse>
-          <span className="text-sm tracking-tighter font-semibold">
-            Heart-healthy
-          </span>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,30 @@
 import { Search } from "lucide-react";
 import RecipeCard from "../RecipeCard";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [error, setError] = useState(null);
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
+      );
+      const data = await res.json();
+      console.log(data);
+      setRecipes(data.meals);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="bg-[#faf9fb] p-10 flex-1">
       <div className="max-w-screen-lg mx-auto">
@@ -10,28 +33,41 @@ const HomePage = () => {
             Search for recipes
           </label>
           <div className="flex items-center gap-2 border-[1px] border-[#E5E7EB] rounded-lg p-2">
-            <Search
-              size="20"
-            />
+            <Search size="20" />
             <input
               type="text"
               id="search"
               placeholder="Search for recipes"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  fetchRecipes(searchQuery);
+                }
+              }}
               className="w-full p-2 pl-10 focus:outline-none focus:border-blue-500"
             />
           </div>
         </form>
-        <h1 className="font-bold text-3cl md:text-5xl mt-4">Recommended Recipes</h1>
-        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">Popular choices</p>
+        <h1 className="font-bold text-3cl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular choices
+        </p>
         <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
+          {loading && <div className="col-span-full">Loading...</div>}
+          {error && <div className="col-span-full text-red-500">{error}</div>}
+          {!loading && recipes.length === 0 && (
+            <div className="col-span-full">No recipes found</div>
+          )}
+          {!loading &&
+            recipes.map((recipe) =>
+              recipe.strYoutube || recipe.strSource ? (
+                <RecipeCard key={recipe.idMeal} recipe={recipe} />
+              ) : null
+            )}
         </div>
       </div>
     </div>
